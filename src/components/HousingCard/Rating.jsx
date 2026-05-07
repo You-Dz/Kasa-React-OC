b44e4e5fbd5e20047988fe2291cf0521ed6697ff
Yoann Dz <recodvz@gmail.com>
2026-05-07 16:12:36 +0200

    Some responsive corrections

--- src/components/HousingCard/Rating.jsx
+++ src/components/HousingCard/Rating.jsx
@@ -1,5 +1,6 @@
 import ActiveStar from "../../assets/icons/active-star.svg"
 import InactiveStar from "../../assets/icons/inactive-star.svg"
+import "./Rating.scss"
 
 function Rating ({rating}) {
     const stars = [0,1,2,3,4];
